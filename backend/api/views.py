c9675b595cc6a7aa51dbb029ae559b605d839dba
Jay Ghevariya <jay.b.ghevariya@gmail.com>
2022-12-15 01:43:59 +0530

    removed comments

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,18 +7,6 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
-# Create your views here.
-
-# class DataAddToDB(request, *args, **kwargs):
-#     if request.method == 'POST':
-#         data = request.POST.copy()
-#         serializer = DataSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return render(request, 'add.html', {'message': 'Data saved successfully!'})
-#         else:
-#             return render(request, 'add.html', {'message': 'Error saving data!'}
-
 
 def add(request,format=None):
     # print("ascas")
@@ -32,13 +20,7 @@ def add(request,format=None):
             return HttpResponse("Data saved successfully!")
         else:
             return HttpResponse("Error saving data!")
-    
-
-# def hello(self):
-#     return HttpResponse("Hello World")
 
 def show_data(self):
     data = Data.objects.all()
     return HttpResponse(data)
- 
-
